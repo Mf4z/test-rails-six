5cef2182d5d6d9f5ce6ba710d7c4a03dbc761d37
Marwan Mai <emef4z@gmail.com>
2024-11-16 21:09:48 +0100

    Update dependencies for prod

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+const { environment } = require("@rails/webpacker");
+
+const babelLoader = environment.loaders.get("babel");
+babelLoader.use[0].options.presets = [
+  ["@babel/preset-env", { modules: false }],
+];
+babelLoader.use[0].options.plugins = [
+  "@babel/plugin-proposal-private-methods",
+  "@babel/plugin-proposal-class-properties",
+];
+
+module.exports = environment;
